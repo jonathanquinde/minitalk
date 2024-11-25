5f7373f1c348b0402582485bb17f1e3ad70c11e9
Jonathan Quinde <jon.quindepaz@gmail.com>
2024-11-25 15:46:43 +0100

    feat: Añadida implementacion para que mande en binario el primer caracter de str

--- client.c
+++ client.c
@@ -1,8 +1,7 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <signal.h> 
-#include <unistd.h> 
-#include <string.h> 
+#include "header.h"
+
+void    to_binary(int digit, int server_pid);
+void    fill_zero(int digit, int server_pid);
 
 int main(int argc, char *argv[])
 {
@@ -11,16 +10,47 @@ int main(int argc, char *argv[])
 
 	if (argc != 3)
 	{
-		printf("Numero de argumentos invalidos");
+		ft_printf("Numero de argumentos invalidos");
 		return (0);
 	}
 	str = argv[2];
 	server_pid = atoi(argv[1]);
 	if (server_pid == 0)
 	{
-		printf("PID del servidor invalido");
+		ft_printf("PID del servidor invalido");
 		return (0);
 	}
-	(void) str;
-	kill(server_pid, 10);	
+	fill_zero((int) *str, server_pid);
+	to_binary((int) *str, server_pid);
+}
+
+void    fill_zero(int digit, int server_pid)
+{
+    int number_zero;
+
+    number_zero = 8;
+    while (digit != 0)
+    {
+        digit /= 2;
+        number_zero--;
+    }
+    while (number_zero)
+    {
+		kill(server_pid, 12);
+        number_zero--;
+    }
+}
+
+void    to_binary(int digit, int server_pid)
+{
+	if (digit >= 2)
+		to_binary(digit/2, server_pid);
+	if (digit % 2 == 0)
+	{
+		kill(server_pid, 12);
+	}
+	else
+	{
+		kill(server_pid, 10);
+	}
 }
