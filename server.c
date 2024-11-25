d4d9f86e9dc9ea57b52817581c7dddd130d85b21
Jonathan Quinde <jon.quindepaz@gmail.com>
2024-11-25 15:45:39 +0100

    feat: server: Añadidas funciones para la señaes USR1 y USR2

--- server.c
+++ server.c
@@ -1,24 +1,29 @@
-#include <stdio.h>
-#include <unistd.h>
-#include <signal.h>
+#include "header.h"
 
-void	handler(int x);
+void	handle_usr1(int signum);
+void	handle_usr2(int signum);
 
 int	main(void)
 {
 	int pid;
 
-	signal(10, handler);
 	pid = getpid();
+	ft_printf("El servidor esta corriendo, PID:%d\n--------------------------\n", pid);
+	signal(10, handle_usr1);
+	signal(12, handle_usr2);
 	while(1)
 	{
-		printf("Server is running\nPID:%d\n------------------\n", pid);
-		sleep(5);
+		;
 	}
 }
 
-void	handler(int x)
+void	handle_usr1(int signum)
 {
-	x = 0;
-	printf("hola %d\n", x);
+	(void)signum;
+	write(1, "1", 1);
+}
+void	handle_usr2(int signum)
+{
+	(void)signum;
+	write(1, "0", 1);
 }
