fa7d58e5a8e59f899b9c60ceaec324ca086008b6
Jonathan Quinde <jonquindepaz@gmail.com>
2024-11-27 11:15:38 +0100

    feat: client: Añadida send_sting para procesar la string feat: server: Guarda cada señal como un bit en una string statica, cuando llegue a 8 sumara los bits para crear un byte, que sera printeado

--- client.c
+++ client.c
@@ -1,7 +1,8 @@
 #include "header.h"
 
-void    to_binary(int digit, int server_pid);
-void    fill_zero(int digit, int server_pid);
+void    to_binary(unsigned char ch, int server_pid);
+void    fill_zero(unsigned char ch, int server_pid);
+void	send_string(char	*str, int server_pid);
 
 int main(int argc, char *argv[])
 {
@@ -11,46 +12,59 @@ int main(int argc, char *argv[])
 	if (argc != 3)
 	{
 		ft_printf("Numero de argumentos invalidos");
-		return (0);
+		return (1);
 	}
 	str = argv[2];
 	server_pid = atoi(argv[1]);
 	if (server_pid == 0)
 	{
 		ft_printf("PID del servidor invalido");
-		return (0);
+		return (1);
 	}
-	fill_zero((int) *str, server_pid);
-	to_binary((int) *str, server_pid);
+	send_string(str, server_pid);
 }
 
-void    fill_zero(int digit, int server_pid)
+void	send_string(char *str, int server_pid)
+{
+	while (*str)
+	{
+		fill_zero(*str, server_pid);
+		to_binary(*str, server_pid);
+		str++;
+	}
+	fill_zero(*str, server_pid);
+}
+
+void    fill_zero(unsigned char ch, int server_pid)
 {
     int number_zero;
 
     number_zero = 8;
-    while (digit != 0)
+    while (ch != 0)
     {
-        digit /= 2;
+        ch /= 2;
         number_zero--;
     }
     while (number_zero)
     {
 		kill(server_pid, 12);
+		usleep(300);
         number_zero--;
     }
 }
 
-void    to_binary(int digit, int server_pid)
+void    to_binary(unsigned char ch, int server_pid)
 {
-	if (digit >= 2)
-		to_binary(digit/2, server_pid);
-	if (digit % 2 == 0)
+	if (ch >= 2)
+		to_binary(ch/2, server_pid);
+	if (ch % 2 == 0)
 	{
 		kill(server_pid, 12);
+		usleep(300);
 	}
 	else
 	{
 		kill(server_pid, 10);
+		usleep(300);
 	}
 }
--- server.c
+++ server.c
@@ -1,29 +1,79 @@
 #include "header.h"
 
-void	handle_usr1(int signum);
-void	handle_usr2(int signum);
+void	handle(int signum);
+void	build_byte(int bit);
+void	bits_to_char(char byte[8]);
 
 int	main(void)
 {
-	int pid;
+	int 				pid;
+	struct sigaction	sa_usr1;
+	struct sigaction	sa_usr2;
 
+	sigemptyset(&sa_usr1.sa_mask);
+	sigemptyset(&sa_usr2.sa_mask);
+	sa_usr1.sa_flags = 0;
+	sa_usr2.sa_flags = 0;
+	sa_usr1.sa_handler = handle;
+	sa_usr2.sa_handler = handle;
 	pid = getpid();
 	ft_printf("El servidor esta corriendo, PID:%d\n--------------------------\n", pid);
-	signal(10, handle_usr1);
-	signal(12, handle_usr2);
-	while(1)
+	sigaction(10, &sa_usr1, NULL);
+	sigaction(12, &sa_usr2, NULL);
+	while (1)
 	{
 		;
 	}
 }
 
-void	handle_usr1(int signum)
+void	bits_to_char(char byte[8])
 {
-	(void)signum;
-	write(1, "1", 1);
+	unsigned char	ch;
+
+	ch = 0;
+	if (byte[0] == 1)
+		ch += 128;
+	if (byte[1] == 1)
+		ch += 64;
+	if (byte[2] == 1)
+		ch += 32;
+	if (byte[3] == 1)
+		ch += 16;
+	if (byte[4] == 1)
+		ch += 8;
+	if (byte[5] == 1)
+		ch += 4;
+	if (byte[6] == 1)
+		ch += 2;
+	if (byte[7] == 1)
+		ch += 1;
+	ft_printf("%c", ch);
+	if (ch == 0)
+		ft_printf("\n");
 }
-void	handle_usr2(int signum)
+
+void	build_byte(int bit)
 {
-	(void)signum;
-	write(1, "0", 1);
+	static char byte[8];
+	static int	count;
+	
+	byte[count] = bit;
+	count++;
+	if (count == 8)
+	{
+		count = 0;
+		bits_to_char(byte);
+	}
+}
+
+void	handle(int signum)
+{
+	if (signum == 10)
+	{
+		build_byte(1);
+	}
+	else if (signum == 12)
+	{
+		build_byte(0);
+	}
 }
